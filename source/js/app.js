c825b01310d9157d32121b88911ab6a0f184c995
Azune-NK <w1ndy@inbox.ru>
2019-08-03 17:31:49 +0300

    Сделал шапку, todo tablet+scroll

---
+++ source/js/app.js
@@ -0,0 +1,20 @@
+var pageHeader = document.querySelector('.page-header'),
+    navMain = document.querySelector('.main-nav'),
+    navToggle = document.querySelector('.main-nav__toggle');
+
+pageHeader.classList.remove('page-header--white');
+navMain.classList.remove('main-nav--opened');
+navMain.classList.remove('main-nav--nojs');
+navMain.classList.add('main-nav--closed');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    pageHeader.classList.add('page-header--white');
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    pageHeader.classList.remove('page-header--white');
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
