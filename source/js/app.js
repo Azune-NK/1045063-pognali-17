26af8f97a8e009eeb428b133eca1565828bffcbd
Azune-NK <w1ndy@inbox.ru>
2019-08-14 21:26:58 +0300

    +блок country-filter

--- source/js/app.js
+++ source/js/app.js
@@ -3,13 +3,46 @@ var pageHeader = document.querySelector(".page-header"),
     navToggle = document.querySelector(".main-nav__toggle"),
     bizPricingModal = document.querySelector(".modal"),
     bizPricingOpen = document.querySelector(".pricing__btn"),
-    bizPricingClose = document.querySelector(".biz-pricing__button");
+    bizPricingClose = document.querySelector(".biz-pricing__button"),
+    filterBtns = document.querySelectorAll('.companions-filter__legend-btn');
 
 pageHeader.classList.remove("page-header--white");
 pageHeader.classList.remove("page-header--nojs");
 navMain.classList.remove("main-nav--opened");
 navMain.classList.remove("main-nav--nojs");
 navMain.classList.add("main-nav--closed");
+
+/* (function(e){
+  e.closest = e.closest || function(css){
+    var node = this;
+
+    while (node) {
+       if (node.matches(css)) return node;
+       else node = node.parentElement;
+    }
+    return null;
+  }
+ })(Element.prototype); */
+
+ /* for (var i = 0; i < filterBtns.length; i++) {
+  filterBtns[i].addEventListener('click', function(e){
+  e.preventDefault();
+  var target = e.target.closet('.companions-filter__food');
+  if (!target) return;
+
+  target.querySelector('.companions-filter__list').classList.add(.companions-filter__list--opened)
+  })
+  } */
+
+/* var blocks = document.querySelectorAll('.companions-filter__food');
+for (var i = 0; i < blocks.length; i++) {
+blocks[i].addEventListener('click', function(e){
+e.preventDefault();
+if (!e.target.classList.contains('companions-filter__legend-btn')) return;
+
+this.querySelector('.companions-filter__list').classList.add(.companions-filter__list--opened)
+})
+} */
 
 navToggle.addEventListener("click", function (/* Вопрос почему тут работает без названия функции, а когда функции ниже тоже были без названия - они не работали? */) {
   if (navMain.classList.contains("main-nav--closed")) {
