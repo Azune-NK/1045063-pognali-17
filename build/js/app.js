e76e4f3a6a603b0d15eeefd3e938312303fc7f43
Azune-NK <w1ndy@inbox.ru>
2020-01-28 14:01:12 +0300

    Убирает /build из gitignore

---
+++ build/js/app.js
@@ -0,0 +1,80 @@
+var pageHeader = document.querySelector(".page-header"),
+  navMain = document.querySelector(".main-nav"),
+  navToggle = document.querySelector(".main-nav__toggle"),
+  bizPricingModal = document.querySelector(".modal"),
+  bizPricingOpen = document.querySelector(".pricing__btn"),
+  bizPricingClose = document.querySelector(".biz-pricing__button"),
+  filterBtns = document.querySelectorAll(".companions-filter__legend-btn");
+
+pageHeader.classList.remove("page-header--white");
+pageHeader.classList.remove("page-header--nojs");
+navMain.classList.remove("main-nav--opened");
+navMain.classList.remove("main-nav--nojs");
+navMain.classList.add("main-nav--closed");
+
+/* (function(e){
+  e.closest = e.closest || function(css){
+    var node = this;
+
+    while (node) {
+       if (node.matches(css)) return node;
+       else node = node.parentElement;
+    }
+    return null;
+  }
+ })(Element.prototype); */
+
+/* for (var i = 0; i < filterBtns.length; i++) {
+filterBtns[i].addEventListener('click', function(e){
+e.preventDefault();
+var target = e.target.closet('.companions-filter__food');
+if (!target) return;
+
+target.querySelector('.companions-filter__list').classList.add("companions-filter__list--closed")
+})
+} */
+
+/* var blocks = document.querySelectorAll('.companions-filter__section');
+for (var i = 0; i < blocks.length; i++) {
+blocks[i].addEventListener('click', function(e){
+e.preventDefault();
+if (!e.target.classList.contains('companions-filter__legend-btn')) return;
+
+this.querySelector('.companions-filter__list').classList.add("companions-filter__list--closed")
+})
+} */
+
+navToggle.addEventListener("click", function () {
+  if (navMain.classList.contains("main-nav--closed")) {
+    pageHeader.classList.add("page-header--white");
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+  } else {
+    pageHeader.classList.remove("page-header--white");
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+  }
+});
+
+if (bizPricingOpen) {
+  bizPricingOpen.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    bizPricingModal.classList.add("modal--opened");
+    bizPricingClose.focus();
+  });
+}
+
+if (bizPricingClose) {
+  bizPricingClose.addEventListener("click", function (evt) {
+    bizPricingModal.classList.remove("modal--opened");
+  });
+}
+
+document.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (bizPricingModal.classList.contains("modal--opened")) {
+      bizPricingModal.classList.remove("modal--opened");
+    }
+  }
+});
